30c278f650c872a16a3ae2ca39e13fa7ab7f3353
Denis Nikolaev <idenisnikolaev@gmail.com>
2015-08-05 13:46:39 +0300

    restructured project

--- 12843/solution/PathBuilder/src/pathbuilder/PathBuilderTestDrive.java
+++
@@ -1,8 +0,0 @@
-package pathbuilder;
-
-public class PathBuilderTestDrive {
-    public static void main(String[] args) {
-        PathBuilderGUI pbg = new PathBuilderGUI();
-        pbg.buildGUI();
-    }
-}
